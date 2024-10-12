147075a4b6a731314d01d70ae02d6d36c75366f6
uditrajsingh <uditr.singh@decisionpoint.in>
2024-10-12 14:11:31 +0530

    updated-files

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 const cors = require('cors');
 const app = express();
-const PORT = 3001;
+const port = 3001;
 
 app.use(express.json());
 app.use(cors());
